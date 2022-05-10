81f37794dde57a4bcaa6b6760eaf608dcb39d269
Olayinka247 <97066655+Olayinka247@users.noreply.github.com>
2022-05-10 15:21:36 +0100

    json file created

--- src/apis/authors/index.js
+++ src/apis/authors/index.js
@@ -2,12 +2,20 @@ import express from "express";
 
 const authorsRouter = express.Router();
 
-authorsRouter.post("/", (request, response) => {});
+authorsRouter.post("/", (request, response) => {
+  response.send({ message: "POSTED" });
+});
 authorsRouter.get("/", (request, response) => {
   response.send({ message: "Router Created" });
 });
-authorsRouter.get("/:authorId", (request, response) => {});
-authorsRouter.put("/:authorsId", (request, response) => {});
-authorsRouter.delete("/:authorsId", (request, response) => {});
+authorsRouter.get("/:authorId", (request, response) => {
+  response.send({ message: "Created BY ID" });
+});
+authorsRouter.put("/:authorsId", (request, response) => {
+  response.send({ message: "Edited" });
+});
+authorsRouter.delete("/:authorsId", (request, response) => {
+  response.send({ message: "Deleted" });
+});
 
 export default authorsRouter;
